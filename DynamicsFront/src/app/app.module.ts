6d0868e0a5434872128a715fa215207391d46e03
Lorenzo López Rodríguez <lorenlr14@gmail.com>
2023-08-03 14:14:59 +0200

    Componentes y servicio

--- DynamicsFront/src/app/app.module.ts
+++ DynamicsFront/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
